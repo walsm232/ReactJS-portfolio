fccd5e4eb1e712089686a587c81da627c7f83efc
Michael <michael.walsh232@mail.dcu.ie>
2021-09-03 11:10:26 +0100

    Changed headshot image and added react-particles to work with the header background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
       <Header />
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,14 @@ const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <img className="headshot" src={headshot} alt="Michael Walsh Headshot Image"/>
+                <img className="headshot" src={headshot} alt="Michael Walsh Headshot Image" width="200px" height="200px"/>
                 <h1>Michael Walsh</h1>
                 <Typed 
                     className="typed-text"
                     strings={["Software Engineer"]}
                     typeSpeed={100}
+                    backSpeed={30}
+                    loop
                 />
                 <a href="#"><button type="button" className="btn btn-main-offer btn-secondary">Contact Me</button></a>
             </div>
